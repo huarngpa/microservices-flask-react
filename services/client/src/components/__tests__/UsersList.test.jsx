1495d0ebe60bf5b96740dc1f54222ee6ed133899
huarngpa <huarngpa@gmail.com>
2019-04-21 11:02:15 -0500

    (checkpoint) initial commits to makeup microservices repo

---
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import UsersList from '../UsersList';
+
+const users = [
+  {
+    'active': true,
+    'email': 'test1@example.com',
+    'id': 1,
+    'username': 'test1'
+  },
+  {
+    'active': true,
+    'email': 'test2@example.com',
+    'id': 2,
+    'username': 'test2'
+  }
+];
+
+test('UsersList renders properly', () => {
+  const wrapper = shallow(<UsersList users={users} />);
+  const element = wrapper.find('h4');
+  expect(element.length).toBe(2);
+  expect(element.get(0).props.children).toBe('test1');
+});
