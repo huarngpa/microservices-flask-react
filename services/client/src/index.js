3f3d7df5ceb5b1cc83172fc9bd856d73bfcdeffd
huarngpa <huarngpa@gmail.com>
2019-05-05 18:30:02 -0500

    feat(client-docker): client code and docker stuff

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -9,13 +9,32 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   }
 
   addUser(event) {
     event.preventDefault();
-    console.log('sanity check!');
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+    }
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({username: '', email: ''})
+      })
+      .catch((err) => { console.log(err); })
   }
 
   componentDidMount() {
@@ -24,8 +43,8 @@ class App extends Component {
 
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { this.setState({users: res.data.data.users}) })
-    .catch((err) => { console.log(err); });
+      .then((res) => { this.setState({users: res.data.data.users}) })
+      .catch((err) => { console.log(err); });
   }
 
   render() {
@@ -37,7 +56,12 @@ class App extends Component {
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
-              <AddUser addUser={this.addUser}/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <br/><br/>
               <UsersList users={this.state.users} />
             </div>
