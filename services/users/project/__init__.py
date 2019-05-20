69cc30f4042eb01deb75140ca019e5550cc7b211
huarngpa <huarngpa@gmail.com>
2019-05-19 22:55:09 -0500

    feat(jwt): wip for jwt authentication

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,8 +6,6 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-import logging
-import sys
 
 
 # instantiate the db
@@ -36,6 +34,8 @@ def create_app(script_info=None):
     # register blueprints
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
+    from project.api.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     # shell context for flask cli
     @app.shell_context_processor
